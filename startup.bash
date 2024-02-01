03b4f93e8e14cd4a7d4ce633a48e342265f3794e
Design3 Robot <design3-team3@proton.me>
2024-02-01 12:15:47 -0500

    fixed startup.bash

--- startup.bash
+++ startup.bash
@@ -5,5 +5,5 @@ source /home/team0/design3_ws/source_ws.bash
 screen -r -S "bridge" -X quit 2>/dev/null
 screen -r -S "system" -X quit 2>/dev/null
 
-screen -S system -dm bash -c "ros2 launch rosbridge_server rosbridge_websocket_launch.xml"
+screen -S bridge -dm bash -c "ros2 launch rosbridge_server rosbridge_websocket_launch.xml"
 screen -S system -dm bash -c "ros2 launch design3_system system.launch.py"
