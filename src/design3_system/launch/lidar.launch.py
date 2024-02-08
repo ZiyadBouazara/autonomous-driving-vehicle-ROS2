8a982418fba0a7b0822b5c5ed922cd661f15b971
William Fecteau <william-fecteau@hotmail.com>
2024-02-08 10:12:38 -0500

    Running black on existing python code

--- src/design3_system/launch/lidar.launch.py
+++ src/design3_system/launch/lidar.launch.py
@@ -1,31 +1,24 @@
-from ament_index_python import get_package_share_directory
-
-from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription, ExecuteProcess
-from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch_ros.actions import Node
-from launch.substitutions import LaunchConfiguration
-
 import os
 
-def generate_launch_description():    
-    lidar_shared_dir = get_package_share_directory('design3_system')
-    laser_config = os.path.join(lidar_shared_dir, 'config', 'lidar.yaml')
+from ament_index_python import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node
 
-    laser_la = DeclareLaunchArgument(
-        'laser_config',
-        default_value=laser_config
-    )
+
+def generate_launch_description():
+    lidar_shared_dir = get_package_share_directory("design3_system")
+    laser_config = os.path.join(lidar_shared_dir, "config", "lidar.yaml")
+
+    laser_la = DeclareLaunchArgument("laser_config", default_value=laser_config)
 
     laser_node = Node(
-        package='ldlidar_stl_ros2',
-        executable='ldlidar_stl_ros2_node',
-        name='LD19',
-        output='screen',
-        parameters=[LaunchConfiguration('laser_config')],
+        package="ldlidar_stl_ros2",
+        executable="ldlidar_stl_ros2_node",
+        name="LD19",
+        output="screen",
+        parameters=[LaunchConfiguration("laser_config")],
     )
-    
-    return LaunchDescription([
-        laser_la,
-        laser_node
-    ])
+
+    return LaunchDescription([laser_la, laser_node])
--- src/design3_system/launch/teleop.launch.py
+++ src/design3_system/launch/teleop.launch.py
@@ -1,35 +1,27 @@
-from ament_index_python import get_package_share_directory
-
-from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription, ExecuteProcess
-from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch_ros.actions import Node
-from launch.substitutions import LaunchConfiguration
-
 import os
 
-def generate_launch_description():
-    shared_dir = get_package_share_directory('design3_system')
-    teleop_config = os.path.join(shared_dir, 'config', 'teleop.yaml')
+from ament_index_python import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node
 
-    teleop_la = DeclareLaunchArgument(
-        'teleop_config',
-        default_value=teleop_config
-    )
-    
+
+def generate_launch_description():
+    shared_dir = get_package_share_directory("design3_system")
+    teleop_config = os.path.join(shared_dir, "config", "teleop.yaml")
+
+    teleop_la = DeclareLaunchArgument("teleop_config", default_value=teleop_config)
+
     joy_node = Node(
-        package='joy',
-        executable='joy_node',
+        package="joy",
+        executable="joy_node",
     )
 
     teleop_node = Node(
-        package='teleop_twist_joy',
-        executable='teleop_node',
-        parameters=[LaunchConfiguration('teleop_config')],
+        package="teleop_twist_joy",
+        executable="teleop_node",
+        parameters=[LaunchConfiguration("teleop_config")],
     )
 
-    return LaunchDescription([
-        teleop_la,
-        joy_node,
-        teleop_node 
-    ])
+    return LaunchDescription([teleop_la, joy_node, teleop_node])
