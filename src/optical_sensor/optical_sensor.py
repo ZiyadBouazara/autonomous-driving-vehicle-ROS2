cc37b1a21662d15b249405f126f09ae02af1e4f4
Design3 Robot <design3-team3@proton.me>
2024-02-12 16:02:38 -0500

    (Will) Added optical sensor tests

---
+++ src/optical_sensor/optical_sensor.py
@@ -0,0 +1,27 @@
+import Jetson.GPIO as GPIO
+import time
+
+# Pin Definitions
+output_pin = 18  # BOARD pin 12, BCM pin 18
+
+def main():
+    # Pin Setup:
+    # Board pin-numbering scheme
+    GPIO.setmode(GPIO.BCM)
+    # set pin as an output pin with optional initial state of HIGH
+    GPIO.setup(output_pin, GPIO.OUT, initial=GPIO.HIGH)
+
+    print("Starting demo now! Press CTRL+C to exit")
+    curr_value = GPIO.HIGH
+    try:
+        while True:
+            time.sleep(1)
+            # Toggle the output every second
+            print("Outputting {} to pin {}".format(curr_value, output_pin))
+            GPIO.output(output_pin, curr_value)
+            curr_value ^= GPIO.HIGH
+    finally:
+        GPIO.cleanup()
+
+if __name__ == '__main__':
+    main()
